ebd6c194753a0dd68e2b6fc0acefb2dec6e76039
Talha-glitched <m.talha2703@gmail.com>
2026-08-10 17:49:16 +0400

    Microsoft clarity analytics

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,8 +1,11 @@
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import Clarity from '@microsoft/clarity';
 import App from './App.jsx';
 import './styles/shared.css';
 import './styles/app.css';
+
+Clarity.init('y07yl1sf0l');
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
